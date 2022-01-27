04940b27660aa3f1636054a09f3b4f5efed0e34c
Mark Grechanik <drmark@uic.edu>
2022-01-27 13:36:54 -0600

    implementation of arith exp dsl + comleted with sub

--- src/scala/OurArithExpDSL.scala
+++ src/scala/OurArithExpDSL.scala
@@ -16,6 +16,7 @@ object OurArithExpDSL:
     case Value(input: BasicType)
     case Var(name: String)
     case Add(op1: ArithExp, op2: ArithExp)
+    case Sub(op1: ArithExp, op2: ArithExp)
     private val bindingScoping: Map[String, Int] = Map("x"->2, "Adan"->10)
 
     def eval: BasicType =
@@ -23,9 +24,11 @@ object OurArithExpDSL:
         case Value(i) => i
         case Var(name) => bindingScoping(name)
         case Add(op1, op2) => op1.eval + op2.eval
+
+        case Sub(op1, op2) => op1.eval - op2.eval
       }
 
   @main def runArithExp: Unit =
     import ArithExp.*
-    val firstExpression = Add(Add(Value(2), Value(3)),Var("Adan")).eval
+    val firstExpression = Sub(Add(Add(Value(2), Value(3)),Var("Adan")), Var("x")).eval
     println(firstExpression)
